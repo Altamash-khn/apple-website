45f8d5d24bbf9875e2ecb69b99516f0849941f5b
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-05-26 19:34:57 +0530

    add video for hero-section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,39 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState } from "react";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 768 ? smallHeroVideo : heroVideo,
+  );
+  useGSAP(() => {
+    gsap.fromTo(
+      "#hero",
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1 },
+    );
+  }, []);
+
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title" id="hero">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            src={videoSrc}
+            typeof="video/mp4"
+            autoPlay
+            loop
+            muted
+            className="w-full rounded-lg"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
