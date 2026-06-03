56aa41ebb66ab3def7c6903b0d10f9d11dd582e6
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-06-04 04:46:31 +0530

    add first few lines for Model

---
+++ src/components/Model.tsx
@@ -0,0 +1,79 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useRef, useState } from "react";
+import { yellowImg } from "../utils";
+import * as THREE from "three";
+import { setFlagsFromString } from "v8";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+
+const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    colors: ["#8f8a81", "#ffe7b9", "#6f6c64"],
+    img: yellowImg,
+  });
+
+  // camera control for the model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  // Model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
+  useGSAP(() => {
+    gsap.to("#heading", {
+      scrollTrigger: {
+        trigger: "#heading",
+        toggleActions: "restart none restart none",
+      },
+      y: 0,
+      opacity: 1,
+    });
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look
+        </h1>
+
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
+
+            <Canvas>
+              <View.Port></View.Port>
+            </Canvas>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Model;
